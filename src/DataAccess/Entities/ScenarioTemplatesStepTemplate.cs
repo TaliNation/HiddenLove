fdd989dea5c5ec3c02c8d31b63dcc073e2a0f477
Léo <leo.driat@orange.fr>
2021-04-02 15:51:02 +0200

    Création des scénarios

--- src/DataAccess/Entities/ScenarioTemplatesStepTemplate.cs
+++ src/DataAccess/Entities/ScenarioTemplatesStepTemplate.cs
@@ -7,6 +7,7 @@ namespace HiddenLove.DataAccess.Entities
 {
     public partial class ScenarioTemplatesStepTemplate : IEntity<int>
     {
+        public int Id { get; set; }
         public int IdScenariotemplate { get; set; }
         public int IdSteptemplate { get; set; }
         public int? OrderIndex { get; set; }

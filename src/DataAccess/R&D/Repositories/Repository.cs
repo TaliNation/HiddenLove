02765f9c8f8ce9009f04c0b453b43445110ea811
Léo Driat <leo.driat@orange.fr>
2021-04-03 13:08:47 +0200

    R&D sur une nouvelle forme de repository

--- src/DataAccess/R&D/Repositories/Repository.cs
+++ src/DataAccess/R&D/Repositories/Repository.cs
@@ -1,26 +1,31 @@
 using System.Collections.Generic;
+using HiddenLove.DataAccess.Entities;
 using HiddenLove.DataAccess.QueryFactories;
-using HiddenLove.DataAccess.RD.TableAccess;
+using HiddenLove.DataAccess.RD.TableAccesses;
+using HiddenLove.DataAccess.RD.TableInfos;
 using SqlKata.Execution;
 
 namespace HiddenLove.DataAccess.RD.Repositories
 {
 
-    public abstract class Repository
+    public class Repository
     {
         protected QueryFactory QueryFactory { get; private set; } 
 
-        public TableAccess.TableAccess TableAccess { get; private set; }
+        public TableAccessGenerics TableAccess { get; private set; }
         
-        public Repository()
+        public Repository(TableAccessGenerics tableAccess)
         {
             IQueryFactory queryFactory = new ProductionDbQueryFactory();
             QueryFactory = queryFactory.QueryFactory;
+
+            SetTableAccess(tableAccess);
         }
 
-        public Repository(IQueryFactory queryFactory)
+        public Repository(TableAccessGenerics tableAccess, IQueryFactory queryFactory)
         {
             QueryFactory = queryFactory.QueryFactory;
+            SetTableAccess(tableAccess);
         }
 
         public void SetQueryFactory(IQueryFactory queryFactory)
@@ -29,22 +34,20 @@ namespace HiddenLove.DataAccess.RD.Repositories
             TableAccess.SetQueryFactory(QueryFactory);
         }
 
-        public void SetTableAccess(TableAccess.TableAccess tableAccess)
+        public void SetTableAccess(TableAccessGenerics tableAccess)
         {
             TableAccess = tableAccess;
             TableAccess.SetQueryFactory(QueryFactory);
         }
 
-        // public virtual TEntity GetById<TKey, TEntity>(TKey key) =>
 
+        public virtual TEntity GetById<TKey, TEntity>(TKey key) where TEntity : IEntity<TKey> =>
+            TableAccess.GetById<TKey, TEntity>(key);
 
-        // IEnumerable<TEntity> GetAll<TEntity>() =>
+        public virtual IEnumerable<TEntity> GetAll<TKey, TEntity>() where TEntity : IEntity<TKey> =>
+            TableAccess.GetAll<TEntity>();
 
-        // TKey InsertGetKey<TKey, TEntity>(TEntity entity) =>
-
-        // void Insert<TEntity>(TEntity entity) =>
-
-        // TEntity InsertGetEntity<TEntity>(TEntity entity) =>
-
+        public virtual TKey Insert<TKey, TEntity>(TEntity entity) where TEntity : IEntity<TKey> =>
+            TableAccess.Insert<TKey>(entity);
     }
 }
---
+++ src/DataAccess/R&D/TableInfos/ITableInfo.cs
@@ -0,0 +1,9 @@
+namespace HiddenLove.DataAccess.RD.TableInfos
+{
+    public interface ITableInfos
+    {
+        string TableName { get; }
+
+        string PrimaryKeyName { get; }
+    }
+}

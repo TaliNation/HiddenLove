c4919dff86c5823dcab6de360afd895593418bd6
Léo Driat <leo.driat@orange.fr>
2021-04-08 19:38:41 +0200

    Correction des erreurs dans les calculs

--- src/MailService/Program.cs
+++ src/MailService/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
 
 namespace HiddenLove.MailService
 {

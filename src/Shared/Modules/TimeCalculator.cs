7b72ac48a0b9d40bf5dd2c604564b30a75acabdc
Léo Driat <leo.driat@orange.fr>
2021-04-08 19:38:41 +0200

    Correction des erreurs dans les calculs

--- src/Shared/Modules/TimeCalculator.cs
+++ src/Shared/Modules/TimeCalculator.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace HiddenLove.Shared.Modules
 {
     public static class TimeCalculator
     {
-        public static int MillisecondsUntil(DateTime time)
+        public static int MillisecondsUntil(DateTime dateTime)
         {
             TimeSpan time = dateTime - DateTime.Now;
             return (int)time.TotalMilliseconds;

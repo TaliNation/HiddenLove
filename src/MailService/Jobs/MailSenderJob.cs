f53baaa390ae4d87cf367dddfd9f9af38699809d
Léo Driat <leo.driat@orange.fr>
2021-04-13 00:04:53 +0200

    Mise en place du planificateur et du client SMTP

---
+++ src/MailService/Jobs/MailSenderJob.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Quartz;
+
+namespace HiddenLove.MailService.Jobs
+{
+	public class MailSenderJob : IJob
+	{
+		public async Task Execute(IJobExecutionContext context)
+		{
+
+		}
+	}
+}
--- src/MailService/Program.cs
+++ src/MailService/Program.cs
@@ -7,14 +7,51 @@ using System.Text;
 using System.Threading.Tasks;
 using Quartz;
 using Quartz.Impl;
+using Microsoft.Extensions.Configuration;
+using HiddenLove.MailService.Configurations;
+using HiddenLove.MailService.Modules;
+using HiddenLove.Shared.Modules;
 
 namespace HiddenLove.MailService
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+		public static SmtpClient SmtpClient { get; private set; }
+
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var emailSenderConfig = ConfigurationManager.Create<MailSender>();
+			SmtpClient = new SmtpClient(emailSenderConfig.Host, emailSenderConfig.Port)
+			{
+				Credentials = new NetworkCredential(emailSenderConfig.Address, emailSenderConfig.Password),
+				EnableSsl = true
+			};
+
+			var standardSchedulerFactory = new StdSchedulerFactory();
+
+			while(true)
+			{
+				IScheduler scheduler = await standardSchedulerFactory.GetScheduler();
+
+				await scheduler.Start();
+
+				await ScheduleJobs(scheduler);
+
+				// Formule permettant de recommancer le planificateur à minuit pile.
+				int millisecondsUntilMidnight = TimeCalculator.MillisecondsUntil(DateTime.Parse(DateTime.Now.AddDays(1).ToString("yyyy-MM-dd")));
+
+				await Task.Delay(millisecondsUntilMidnight);
+
+				// Une fois minuit atteint
+				await scheduler.Shutdown();
+			}
         }
+
+		private static async Task ScheduleJobs(IScheduler scheduler)
+		{
+			//TODO : récupérer la liste des evenements.
+			//TODO : créer la liste de mails.
+			//TODO : attacher les jobs.
+		}
     }
 }

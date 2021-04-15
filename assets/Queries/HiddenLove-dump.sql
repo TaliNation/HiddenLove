86f5c01511aa94dea1fc687cd2315f8c5753ad54
Salty-Nerkos <Salty-Nerkos@users.noreply.github.com>
2021-04-15 23:40:56 +0200

    Sql Fix Dump

--- assets/Queries/HiddenLove-dump.sql
+++ assets/Queries/HiddenLove-dump.sql
@@ -22,8 +22,6 @@ CREATE TABLE Offers (
     Title VARCHAR(255) NOT NULL,
     Description VARCHAR(3000),
     Image VARCHAR(255),
-    Id_fakespam INTEGER,
-    FOREIGN KEY (Id_fakespam) REFERENCES FakeSpams (id)
   );
 
 /*==============================================================*/
@@ -33,6 +31,8 @@ CREATE TABLE StepTemplates (
   Id INT IDENTITY NOT NULL PRIMARY KEY,
   Title VARCHAR(255) NOT NULL,
   Description VARCHAR(3000)
+  Id_fakespam INTEGER,
+  FOREIGN KEY (Id_fakespam) REFERENCES FakeSpams (id)
 );
 
 /*==============================================================*/

644675490cfee05fd33c7e4dd742410fde091e92
Léo Driat <leo.driat@orange.fr>
2021-04-21 15:16:10 +0200

    Retrait des messages de debug

--- src/Shared/Models/ScenarioView/UserScenarioData.cs
+++ src/Shared/Models/ScenarioView/UserScenarioData.cs
@@ -9,7 +9,7 @@ namespace HiddenLove.Shared.Models
         public DateTime EventDate { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public bool IsFinished => EventDate < DateTime.Now.AddDays(1);
+        public bool IsFinished => EventDate < DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
         public bool IsToday => EventDate == DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
     }
 }

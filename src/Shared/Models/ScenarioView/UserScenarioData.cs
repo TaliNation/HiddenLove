ee1c6e5fcb928eb518816c1b03965838fbc36242
Léo Driat <leo.driat@orange.fr>
2021-04-21 10:10:01 +0200

    Draft de la page d'abonnements

--- src/Shared/Models/ScenarioView/UserScenarioData.cs
+++ src/Shared/Models/ScenarioView/UserScenarioData.cs
@@ -8,7 +8,7 @@ namespace HiddenLove.Shared.Models
         public string Title { get; set; }
         public DateTime EventDate { get; set; }
         public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; } 
+        public DateTime? EndDate { get; set; }
         public bool IsFinished => EventDate < DateTime.Now.AddDays(1);
         public bool IsToday => EventDate == DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
     }
